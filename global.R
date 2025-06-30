c130ffa8415258ae1a7bfbdd8663c13e71622566
Inês <imss.silva@gmail.com>
2025-06-30 10:51:01 +0200

    Add global R file

---
+++ global.R
@@ -0,0 +1,115 @@
+
+# Install and load all necessary packages: --------------------------------
+
+packageList <- c("remotes",
+                 "progress",
+                 "dplyr",
+                 "tidyr",
+                 "data.table",
+                 "stringr",
+                 "lubridate",
+                 "scales",
+                 "units",
+                 "extrafont",
+                 "ggplot2",
+                 "ggtext",
+                 "ggpointdensity",
+                 "ggnewscale",
+                 "ggh4x",
+                 "ggpubr",
+                 "patchwork",
+                 "png",
+                 "grid",
+                 "ctmm",
+                 "hrbrthemes",
+                 "viridis",
+                 "bayestestR",
+                 "combinat",
+                 "purrr",
+                 "here"
+)
+
+packageList_new <- packageList[!(
+  packageList %in% installed.packages()[, "Package"])]
+if (length(packageList_new)) install.packages(packageList_new)
+sapply(packageList, library,
+       character.only = TRUE, logical.return = TRUE)
+
+if (!requireNamespace("movedesign", quietly = TRUE)) {
+  remotes::install_github("ecoisilva/movedesign")
+} else {
+  library("movedesign")
+}
+
+# write.table(packageList, "packages_used.csv",
+#             append = FALSE,
+#             row.names	= FALSE,
+#             col.names = FALSE,
+#             sep = ",")
+
+rm(packageList, packageList_new)
+
+# Functions: --------------------------------------------------------------
+
+quiet <- function(x) {
+  sink(tempfile())
+  on.exit(sink())
+  invisible(force(x))
+}
+
+quiet(sapply(list.files(path = here::here("R", "functions"),
+                        pattern = ".R",
+                        full.names = TRUE), source))
+
+pal <- load_pal()
+
+suppressMessages(extrafont::loadfonts(device = "win"))
+
+# Pre-defined values: -----------------------------------------------------
+
+# Confidence & credible intervals:
+ci <- 0.95
+cri <- 0.89
+alpha <- 1 - ci
+
+# Number of resamples:
+max_samples <- 250
+
+# Steps:
+iter_step <- 1
+
+# Error threshold:
+error_threshold <- 0.05
+
+# Palette for TRUE/FALSE values:
+pal_values <- c("TRUE" = pal$sea, "FALSE" = pal$dgr)
+pal_values_light <- c("TRUE" = pal$sea_l, "FALSE" = pal$dgr_l)
+
+# Sampling schedule option labels:
+option_labels <- c(
+  "1 year 15 minutes" = paste0(
+    "<span style='color: #000000'><b>Option 1</b></span><br>",
+    "<span style='font-size:11px;'>",
+    "1 year, every 15 minutes</span>"), 
+  "6 years 2 hours" = paste0(
+    "<span style='color: #000000'><b>Option 2</b></span><br>",
+    "<span style='font-size:11px;'>",
+    "6 years, every two hours</span>"), 
+  "4 years 1 day" = paste0(
+    "<span style='color: #000000'><b>Option 3</b></span><br>",
+    "<span style='font-size:11px;'>",
+    "4 years, every day</span>"))
+
+# Method labels:
+method_labels <- c(
+  "hr" = paste0(
+    "<b>Home range</b> ", "estimation"), 
+  "ctsd" = paste0(
+    "<b>Speed & distance</b> ", "estimation"))
+
+# Species labels:
+species_labels <- c(
+  "buffalo" = paste0(
+    "African <b>buffalo</b> (<em>Syncerus caffer</em>)"), 
+  "gazelle" = paste0(
+    "Mongolian <b>gazelle</b> (<em>Procapra gutturosa</em>)"))
